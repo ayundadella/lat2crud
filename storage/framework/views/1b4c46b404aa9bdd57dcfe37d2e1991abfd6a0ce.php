3d65a6d722775c3cc7232f4f721952bc9b8d7e96
ayundadella <ayundadella07@gmail.com>
2020-02-01 09:19:29 +0700

    tugascrud

---
+++ storage/framework/views/1b4c46b404aa9bdd57dcfe37d2e1991abfd6a0ce.php
@@ -0,0 +1,49 @@
+<?php $__env->startSection('content'); ?>
+		<h1>Detail Data Pegawai</h1>
+		<?php if(session('sukses')): ?>
+		<div class="alert alert-success" role="alert">
+			<?php echo e(session('sukses')); ?>
+
+		</div>
+		<?php endif; ?>
+		<div class="row">
+			<div class="col-lg-12">
+
+			        <form action="/pegawai/<?php echo e($pegawai->id); ?>/update" method="POST">
+		        		<?php echo e(csrf_field()); ?>
+
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Nama Depan</label>
+					    <input name="nama_depan" disabled="disabled" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan nama depan" value="<?php echo e($pegawai->nama_depan); ?>">
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Nama Belakang</label>
+					    <input name="nama_belakang" disabled="disabled" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan nama belakang" value="<?php echo e($pegawai->nama_belakang); ?>">
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+					    <select name="jenis_kelamin" disabled="disabled" class="form-control" id="exampleFormControlSelect1">
+					      <option value="L" <?php if($pegawai->jenis_kelamin == 'L'): ?> selected <?php endif; ?>>Laki-laki</option>
+					      <option value="P" <?php if($pegawai->jenis_kelamin == 'P'): ?> selected <?php endif; ?>>Perempuan</option>
+					    </select>
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Agama</label>
+					    <input name="agama" disabled="disabled" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan agama" value="<?php echo e($pegawai->agama); ?>">
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleFormControlTextarea1">Alamat</label>
+					    <textarea name="alamat" disabled="disabled" class="form-control" id="exampleFormControlTextarea1" rows="3"><?php echo e($pegawai->alamat); ?></textarea>
+					</div>
+			        </form>
+			    	</div>
+			      </div>
+			  	</div>
+				</div>
+</table>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\lat2crud\resources\views/pegawai/detail.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/957c9ff326658346f888b41ec1b2a76cc9d1896c.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="row">
+			<div class="col-6">
+				<h1>Data Pegawai</h1>
+			</div>
+				<div class="col-6">
+					<button type="button" class="btn btn-primary btn-sm float-right" data-toggle="modal" data-target="#exampleModal">
+			  Tambah Data
+					</button>
+				</div>
+		
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+			  <div class="modal-dialog" role="document">
+			    <div class="modal-content">
+			      <div class="modal-header">
+			        <h5 class="modal-title" id="exampleModalLabel">Tambah Data</h5>
+			        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+			          <span aria-hidden="true">&times;</span>
+			        </button>
+			      </div>
+
+			      <div class="modal-body">
+			        <form action="/pegawai/create" method="POST">
+			        	<?php echo e(csrf_field()); ?>
+
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Nama Depan</label>
+					    <input name="nama_depan" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan nama depan">
+					  </div>
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Nama Belakang</label>
+					    <input name="nama_belakang" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan nama belakang">
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleFormControlSelect1">Pilih Jenis Kelamin</label>
+					    <select name="jenis_kelamin" class="form-control" id="exampleFormControlSelect1">
+					      <option value="L">Laki-laki</option>
+					      <option value="P">Perempuan</option>
+					    </select>
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleInputEmail1">Agama</label>
+					    <input name="agama" type="text" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Masukkan agama">
+					  </div>
+
+					  <div class="form-group">
+					    <label for="exampleFormControlTextarea1">Alamat</label>
+					    <textarea name="alamat" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+					  </div>
+					
+				</div>
+
+			      <div class="modal-footer">
+			        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+			        <button type="button" class="btn btn-primary">Submit</button>
+			        </form>
+			      </div>
+			    </div>
+			  </div>
+			</div>
+
+<table class="table table-hover">
+	<tr>
+		<th>NAMA DEPAN</th>
+		<th>NAMA BELAKANG</th>
+		<th>JENIS KELAMIN</th>
+		<th>AGAMA</th>
+		<th>ALAMAT</th>
+	</tr>
+	<?php $__currentLoopData = $data_pegawai; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $pegawai): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+	<tr>
+		<td><?php echo e($pegawai->nama_depan); ?></td>
+		<td><?php echo e($pegawai->nama_belakang); ?></td>
+		<td><?php echo e($pegawai->jenis_kelamin); ?></td>
+		<td><?php echo e($pegawai->agama); ?></td>
+		<td><?php echo e($pegawai->alamat); ?></td>
+	</tr>
+	<?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+</table>
+</body>
+</html><?php /**PATH C:\xampp\htdocs\lat2crud\resources\views/Pegawai/index.blade.php ENDPATH**/ ?>
