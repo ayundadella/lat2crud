72e3ae5aa587773a675000db604cb9a79da8fc5d
ayundadella <ayundadella07@gmail.com>
2020-04-22 18:58:17 +0700

    implementing datatable action,edit,and index column

--- resources/views/biodata/index.blade.php
+++ resources/views/biodata/index.blade.php
@@ -1,43 +1,21 @@
 @extends("layout")
 
+@push("style")
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.css"/>
+@endpush
+
 @section("content")
-	<h1>Daftar Mahasiswa</h1>
-	<table class="table table-striped table-hover" id="datatable">
-		<thead>
-			<tr>
-				<th>ID</th>
-				<th>NAMA</th>
-				<th>NIM</th>
-				<th>ACTION</th>
-			</tr>
-		</thead>
-		<tbody>
-
-			<br>
-			<a href="/biodata-mahasiswa/create" class="btn btn-info"> + Tambah Data Mahasiswa</a>
-			<br>
+    <h1>Daftar Mahasiswa</h1>
+    		<!-- <a href="/biodata-mahasiswa/create" class="btn btn-info"> + Tambah Data Mahasiswa</a>
+			<br><br>
 			<a href="/biodata-mahasiswa/export_excel" class="btn btn-success my-3" target="_blank">EXPORT EXCEL</a>
+			<br><br> -->
+    {!! $html->table() !!}
 
-			@forelse($mahasiswa as $data) 
-			<tr>
-				<td>{{ $data->id }}</td>
-				<td>{{ $data->name }}</td>
-				<td>{{ $data->nim }}</td>
-				<td>
-					<a href="{{ route('biodata.show', ['id' => $data->id]) }}" 
-						class="btn btn-success">Detail</a>
-					<a href="{{ route('biodata.edit', ['id' => $data->id]) }}" 
-						class="btn btn-warning">Edit</a>
-					<a onclick="return confirm('Apakah Anda yakin?');" class="btn btn-danger" 
 
-					type="button" href="{{ route('biodata.destroy', ['id' => $data->id]) }}">Delete</a>
-				</td>
-			</tr>
-			@empty
-				<tr>
-					<td colspan="4">Data belum tersedia!</td>
-				</tr>
-			@endforelse
-		</tbody>
-	</table>
 @endsection
+
+@push("script")
+    <script type="text/javascript" src="https://cdn.datatables.net/v/bs/dt-1.10.16/datatables.min.js"></script>
+    {!! $html->scripts() !!}
+@endpush
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,65 +1,32 @@
 <!DOCTYPE html>
-<html>
-
+<html lang="en">
 <head>
-	<title>SI Mahasiswa</title>
-	<link href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" rel="stylesheet">
-	<link href="https://cdn.datatables.net/1.10.20/css/dataTables.bootstrap4.min.css" rel="stylesheet">
-	<link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
-</head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SI Mahasiswa</title>
 
+    <!-- CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    
+    @stack("style")
+</head>
 <body>
 
+    <div class="container">
+        <div class="col-md-12" style="margin-top: 15px;">
+            <a href="{{ route('logout') }}">Logout</a>
+        </div>
 
-	<div class="container">
-		<div class="col-md-12">
-			<a href="{{ route('logout') }}">
-			Logout</a>
-	</div>
-	
-	<div class="container">
-
-		@yield('content')
-
+        <div class="col-md-12">
+            @yield("content")
+        </div>
     </div>
 
-<script src="https://code.jquery.com/jquery-3.3.1.slim.js"></script>
+    <!-- JQuery -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Bootstrap script -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
-
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
-
-<script src="https://cdn.datatables.net/1.10.20/js/dataTables.bootstrap4.min.js"></script>
-
-<script type="text/javascript">
-	
-	$(document).ready(function () {
-
-		var table = $('#datatable').DataTable();
-
-		table.on('click', '.detail', function () {
-
-			$tr = $(this).closest('tr');
-			if ($($tr).hasClass('child')) {
-				$tr = $tr.prev('.parent');
-			}
-
-			var data = table.row($tr).data();
-			console.log(data);
-
-			$('#id').val(data[0]);
-			$('#name').val(data[1]);
-			$('#nim').val(data[2]);
-
-			$('#EditModal').modal('show');
-
-		})
-
-	})
-
-</script>
-
+    @stack("script")
 </body>
 </html>
